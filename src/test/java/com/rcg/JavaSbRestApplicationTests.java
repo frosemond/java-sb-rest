b7225e3ed706382be6ecdfdbbf25d97fa99b3f50
Felicia Rosemond <frosemond@cloudbees.com>
2020-11-25 15:00:58 -0500

    initial commit

---
+++ src/test/java/com/rcg/JavaSbRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSbRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
